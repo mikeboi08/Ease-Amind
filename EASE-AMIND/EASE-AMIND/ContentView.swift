abe19a941a71ed59b23b9b7138d2078dd96dcb68
Dante Parker <148378405+King-DIP@users.noreply.github.com>
2024-01-24 14:02:01 -0500

    Add files via upload

---
+++ EASE-AMIND/EASE-AMIND/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EASE-AMIND
+//
+//  Created by Dante Parker on 1/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EASE-AMIND/EASE-AMIND/EASE_AMINDApp.swift
@@ -0,0 +1,17 @@
+//
+//  EASE_AMINDApp.swift
+//  EASE-AMIND
+//
+//  Created by Dante Parker on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct EASE_AMINDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
