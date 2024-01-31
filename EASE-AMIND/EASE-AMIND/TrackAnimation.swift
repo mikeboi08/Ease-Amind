f173b0aba3ca707f1e18c5a007a65eb216828bb6
Jarvis <jarvis011877@gmail.com>
2024-01-30 20:34:05 -0500

    Realigned Zipper View, added Track animation prototype

---
+++ EASE-AMIND/EASE-AMIND/TrackAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  TrackAnimation.swift
+//  EASE-AMIND
+//
+//  Created by Jarvis Murray on 1/29/24.
+//
+
+import SwiftUI
+
+struct TrackAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrackAnimation()
+}
